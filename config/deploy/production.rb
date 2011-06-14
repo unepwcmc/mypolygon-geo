14124c863d91515e314f88934b4f091aee389b86
Simão Belchior de Castro <simaobelchior@gmail.com>
2011-06-14 10:56:37 +0100

    tweaking the capistrano recipe to avoid db setup

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,6 @@
-require 'bundler/capistrano'
+gem 'brightbox', '>=2.3.8'
+require 'brightbox/recipes'
+#require 'bundler/capistrano'
 
 set :application, "mypolygon-geo"
 set :domain, "unepwcmc-004.vm.brightbox.net"
@@ -7,7 +9,7 @@ role :app, "unepwcmc-004.vm.brightbox.net"
 role :web, "unepwcmc-004.vm.brightbox.net"
 role :db, 'unepwcmc-004.vm.brightbox.net', :primary => true
 
-set :user , "rails"
+#set :user , "rails"
 set(:deploy_to) { File.join("", "home", user, application) }
 
 set :scm, :git
@@ -24,3 +26,6 @@ namespace :deploy do
     run "mkdir -p #{release_path}/tmp && touch #{release_path}/tmp/restart.txt"
   end
 end
+
+set :generate_webserver_config, false
+set :skip_db_setup, true
