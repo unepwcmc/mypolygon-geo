0a5c09ac7f04cd9097166d0f018878d65e631a68
strtwtsn <stuart.watson@unep-wcmc.org>
2013-11-26 14:34:25 +0000

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,25 +1,62 @@
-gem 'brightbox', '>=2.3.8'
-require 'brightbox/recipes'
-#require 'bundler/capistrano'
+set :rails_env, "production"
+# Primary domain name of your application. Used in the Apache configs
+set :domain, "unepwcmc-014.vm.brightbox.net"
+## List of servers
+server "unepwcmc-042.vm.brightbox.net", :app, :web, :db, :primary => true
 
 set :application, "mypolygon-geo"
-set :domain, "unepwcmc-004.vm.brightbox.net"
-## List of servers
-role :app, "unepwcmc-004.vm.brightbox.net"
-role :web, "unepwcmc-004.vm.brightbox.net"
-role :db, 'unepwcmc-004.vm.brightbox.net', :primary => true
+set :server_name, "mypolygon-geo.unepwcmc-014.vm.brightbox.net"
+set :sudo_user, "rails"
+set :app_port, "80" 
 
-#set :user , "rails"
-set(:deploy_to) { File.join("", "home", user, application) }
 
-set :scm, :git
-set :repository, "git@github.com:unepwcmc/mypolygon-geo.git"
-set :branch, "master"
-set :scm_username, "unepwcmc-read"
-set :scm_password, "conservation1"
+desc "Configure VHost"
+task :config_vhost do
+vhost_config =<<-EOF
+server {
+  listen 80;
+  
+  client_max_body_size 4G;
+  server_name #{application}.unepwcmc-024.vm.brightbox.net #{application}.sw01.matx.info;
+  keepalive_timeout 5;
+  root #{deploy_to}/public;
+  passenger_enabled on;
+  rails_env production;
+  gzip on;
+  location ^~ /assets/ {
+    expires max;
+    add_header Cache-Control public;
+  }
+  
+  if (-f $document_root/system/maintenance.html) {
+    return 503;
+  }
 
-set :scm_verbose, true
-default_run_options[:pty] = true
+  error_page 500 502 504 /500.html;
+  location = /500.html {
+    root #{deploy_to}/public;
+  }
+
+  error_page 503 @maintenance;
+  location @maintenance {
+    rewrite  ^(.*)$  /system/maintenance.html break;
+  }
+}
+EOF
+put vhost_config, "/tmp/vhost_config"
+sudo "mv /tmp/vhost_config /etc/nginx/sites-available/#{application}"
+sudo "ln -s /etc/nginx/sites-available/#{application} /etc/nginx/sites-enabled/#{application}"
+end
+
+after "deploy:setup", :config_vhost
+ 
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
 
 namespace :deploy do
   task :start, :roles => :app do
@@ -33,5 +70,3 @@ namespace :deploy do
     deploy.start
   end
 end
-
-set :generate_webserver_config, false
