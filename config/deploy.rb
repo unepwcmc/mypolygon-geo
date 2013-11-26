36cb9bc843758313977b7ded3b46c9d394dc7cc7
strtwtsn <stuart.watson@unep-wcmc.org>
2013-11-26 14:34:53 +0000

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,2 +1,9 @@
 require 'capistrano/ext/multistage'
 set :default_stage, 'staging'
+
+set :scm, :git
+set :repository, "git@github.com:unepwcmc/mypolygon-geo.git"
+set :branch, "master"
+set :scm_username, "unepwcmc-read"
+
+
